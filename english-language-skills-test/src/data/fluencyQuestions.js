a77a334380ff8bbbf30d57472963d7d2449fad40
Keogh9 <999recruitmentready@gmail.com>
2025-06-17 14:20:41 +0100

    Create fluencyQuestions.js

---
+++ english-language-skills-test/src/data/fluencyQuestions.js
@@ -0,0 +1,30 @@
+export const fluencyQuestions = [
+  {
+    id: 'f1',
+    prompt: 'Where did you get the money ____?',
+    options: ['for', 'out', 'from', 'away'],
+    correct: 'from',
+    rationale: "‘Get money from’ indicates the source."
+  },
+  {
+    id: 'f2',
+    prompt: 'If ____ snowing, we will go skiing.',
+    options: ['will', 'it is', 'it was', 'it will'],
+    correct: 'it is',
+    rationale: "In a first conditional, use ‘if it is snowing’."
+  },
+  {
+    id: 'f3',
+    prompt: 'I offered several times, but ____ was interested.',
+    options: ['nobody', 'anybody', 'someone', 'everybody'],
+    correct: 'nobody',
+    rationale: "Negative context requires ‘nobody’."
+  },
+  {
+    id: 'f4',
+    prompt: '____ knows what the future holds.',
+    options: ['Anybody', 'Nobody', 'Somebody', 'Everybody'],
+    correct: 'Nobody',
+    rationale: "‘Nobody knows’ expresses uncertainty about the future."
+  }
+];
